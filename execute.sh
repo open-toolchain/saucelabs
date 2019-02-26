317b198a061ee6bdb5894b5dd0977af4c799e76e
SidneyJiang <46690334+SidneyJiang@users.noreply.github.com>
2019-02-26 14:45:01 -0500

    make apt update and install quite
    in the correct way

--- execute.sh
+++ execute.sh
@@ -46,7 +46,7 @@ fi
 debugme echo "SAUCE_ACCESS_KEY: ${SAUCE_ACCESS_KEY}"
 debugme echo "USER_ID: ${SAUCE_USERNAME}"
 
-which pip || (sudo apt update -q; sudo apt install -y -q python-pip)
+which pip || (sudo apt -qq update; sudo apt -y -qq install python-pip)
 pip install -q requests
 
 cmd_choice=$CMD_CHOICE
